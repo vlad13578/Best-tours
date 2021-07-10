0ed9d70c33c76b507d1abe25cb5b0e2cb6421e34
vlad13578 <61629420+vlad13578@users.noreply.github.com>
2021-07-10 14:46:28 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,4 +1,38 @@
 
+/* Підключення слайдера */
+$(document).ready(function(){
+	$('.slider').slick({
+		centerMode: true,
+		centerPadding: '100px',
+		slidesToShow: 3,
+		slidesToScroll: 1,
+		autoplay: true,
+		autoplaySpeed: 5000,
+		dots: true,
+		pauseOnDotsHover: true,
+		responsive: [
+		  {
+			breakpoint: 1400,
+			settings: {
+			  arrows: true,
+			  centerMode: false,
+			  centerPadding: '40px',
+			  slidesToShow: 2
+			}
+		  },
+		  {
+			breakpoint: 1027,
+			settings: {
+			  arrows: false,
+			  centerMode: false,
+			  centerPadding: '50px',
+			  slidesToShow: 2
+			}
+		  }
+		]
+	});
+});
+/* Бургер меню */
 (function () {
     const burgerItem = document.querySelector('.burger');
     const menu = document.querySelector('.header_nav');
@@ -10,11 +44,5 @@
     menuCloseItem.addEventListener('click', () => {
         menu.classList.remove('header_nav_active');
     });
-    if (window.innerWidth <= 767) {
-        for (let i = 0; i < menuLinks.length; i += 1) {
-            menuLinks[i].addEventListener('click', () => {
-                menu.classList.remove('header_nav_active');
-            });
-        }
-    }
 }());
+
